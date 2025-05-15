9a0ad510c85136ca5bf5af45d40fbd511acacc76
Shakwat18 <shakwathossen70@gmail.com>
2025-05-15 21:04:00 +0600

    change

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,19 @@
-/* eslint-disable react/prop-types */
+import Product from "./Product"
+
+
+const Products =(props)=>{
+const{products}= props
+// console.log(products)
+return(
+    <div className="products">
+      {products.map((product)=>{
+    return(
+           <Product key={product.id} id={product.id} img={product.image} title={product.title} description={product.description} price={product.price} rating={product.rating} category={product.category} />
+    )
+      })}
+    </div>
+)
+
+}
+
+export default Products
